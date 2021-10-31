f5f59f8a781b1c318a0cb96fcb8c984bed1432fe
Joenix <joenix@qq.com>
2021-10-31 14:55:56 +0800

    a

--- vue-ssr-pie-project/index.js
+++
@@ -1,7 +0,0 @@
-// module.exports = ({ Vue, Store, Router }, next) => {
-//     console.log("in Project, Before Render =>", Vue);
-
-//     next(({ Vue, Store, Router }) => {
-//         console.log("in Project, After Render =>", Vue);
-//     });
-// };
